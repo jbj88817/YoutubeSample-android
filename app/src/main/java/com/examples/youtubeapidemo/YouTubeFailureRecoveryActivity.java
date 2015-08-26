67612b94d3854978876f5ff91839aab6be12d945
Bojie Jiang <jiangbojie@gmail.com>
2015-08-26 11:31:35 -0700

    Working example

---
+++ app/src/main/java/com/examples/youtubeapidemo/YouTubeFailureRecoveryActivity.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2012 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.examples.youtubeapidemo;
+
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+
+/**
+ * An abstract activity which deals with recovering from errors which may occur during API
+ * initialization, but can be corrected through user action.
+ */
+public abstract class YouTubeFailureRecoveryActivity extends YouTubeBaseActivity implements
+    YouTubePlayer.OnInitializedListener {
+
+  private static final int RECOVERY_DIALOG_REQUEST = 1;
+
+  @Override
+  public void onInitializationFailure(YouTubePlayer.Provider provider,
+      YouTubeInitializationResult errorReason) {
+    if (errorReason.isUserRecoverableError()) {
+      errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+    } else {
+      String errorMessage = String.format(getString(R.string.error_player), errorReason.toString());
+      Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+    }
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == RECOVERY_DIALOG_REQUEST) {
+      // Retry initialization if user performed a recovery action
+      getYouTubePlayerProvider().initialize(DeveloperKey.DEVELOPER_KEY, this);
+    }
+  }
+
+  protected abstract YouTubePlayer.Provider getYouTubePlayerProvider();
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+}
